d85296541cd686b0e5e35a3ed11934a405dac4ee
nhansp <nhan.094.vn@gmail.com>
2024-03-21 22:37:15 +0700

    update

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  const names = ["Le Phuoc Minh Quan", "Nguyen Tien Hung", "Trinh Thang"]
+  let i;
+  let slides = document.getElementsByClassName("slideshow_image");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-  
+  slides[slideIndex-1].style.display = "block";
+  document.getElementById("usrin4").innerHTML = names[slideIndex-1];
+}
+
+// TODO: merge to [nhansp][a3k55_yearbook]
+//       pull request this.

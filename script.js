eb1a467efecbd1a32acfb01a0ff6e16e989f2dfc
nhansp <nhan.094.vn@gmail.com>
2024-03-23 00:33:24 +0700

    q

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ function plusSlides(n) {
 }
 
 function showSlides(n) {
-  const names = ["Le Phuoc Minh Quan", "Nguyen Tien Hung", "Trinh Thang"]
+  const names = ["Le Phuoc Minh Quan", "Nguyen Tien Hung", "Trinh Thang", "Nguyen Le Phuoc Duy", "Tran Trung Nhan"];
+  const dob = ["16/08/2006", "09/09/2006", "15/12/2006", "15/04/2006", "03/11/2006"];
   let i;
   let slides = document.getElementsByClassName("slideshow_image");
   if (n > slides.length) {slideIndex = 1}
@@ -16,6 +17,7 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   document.getElementById("usrin4").innerHTML = names[slideIndex-1];
+  document.getElementById("usrdob").innerHTML = "Date of birth: " + dob[slideIndex-1];
 }
 
 // TODO: merge to [nhansp][a3k55_yearbook]
